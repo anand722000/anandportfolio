c3ff62a6fadb84f8817ae5f5641e92f9ef89f7f8
Anand Singh <59029929+anand722000@users.noreply.github.com>
2022-11-08 20:13:17 +0530

    Add files via upload

---
+++ userData/projecthelper.js
@@ -0,0 +1,193 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Portfolio Website",
+    desc: "My Protfolio Website. A tempting UX/UI inspired by the top Portfolios. You are already there !",
+    image: "/images/projects/portfolio.jpg",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 2,
+    name: "Yelp Camp",
+    desc: "A Project based on MERN tech stack Ejs instead of React with SDks like mapbox gl and Cloudinary. One of the top full stack Web development project ever Made !!",
+    image: "/images/projects/yelpcamp.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 4,
+    name: "Food Ordering App",
+    desc: "An app based on React.js, Routes as well as payment integration. It is still under development but do check it out.",
+    image: "/images/projects/food-app.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 5,
+    name: "Project Visibility",
+    desc: "This project is the first step to ensure that the Websites are universally accessible and are not restricted to the defined contours of the user website interactions",
+    image:
+      "https://user-images.githubusercontent.com/55958223/130330130-34bf4ffc-cd60-4fc8-87e0-5ac3f455086b.jpeg",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 6,
+    name: "Online Notes Generator",
+    desc: "An Online notes generator based on PHP, MYSQli. It has every authentication functionality with email verification to remember Me. PHP is awesome !!",
+    image: "/images/projects/onlineNotesGenerator.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 7,
+    name: "Online Transaction",
+    desc: "Banking System based on PHP and MySQLi database. Storing all Customer transactions & responsive payments.",
+    image: "/images/projects/onlinebanking.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 8,
+    name: "NGO (Payment Gateway System)",
+    desc: "It's a Ngo payment gatway website with credit card/paypal sandbox API. Payment details are sent to user via Email.",
+    image: "/images/projects/paymentgateway.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 9,
+    name: "Pepper Email Template",
+    desc: "It's a fully responsive email template ready to hit your inbox. It has beautiful UX/UI.",
+    image: "/images/projects/pepper.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 10,
+    name: "Expense handler (React)",
+    desc: "Expense handler based on React. It has expense bars as well year, month filter. A lot of useState !!",
+    image: "/images/projects/expensehandler.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 11,
+    name: "Movie Fetch & Store (Firebase)",
+    desc: "A prefect guide to Restful routes of how firebase storage works with Axios for CRUD requests",
+    image: "/images/projects/movieStorage.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 12,
+    name: "Mobile Shop",
+    desc: "A Mobile Shop website with multiple categories filter functionality and much more. It's lovely isn't it",
+    image: "/images/projects/mobileshop.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 13,
+    name: "Monster Rolodex (React)",
+    desc: "A React Project using Monster Api. Where you can select your fav Monster",
+    image: "/images/projects/monsterrolodex.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 14,
+    name: "Express + MongoDB + Mongoose",
+    desc: "It contains all my express, Mongo and Mongoose projects. Little UI but if you like CRUD and Restful Routes. This is it!!",
+    image: "/images/projects/expressmongoose.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 15,
+    name: "Stop Watch App",
+    desc: "It is an Accurate Stop Watch App with lap timer functionality. It has quick Reset too",
+    image: "/images/projects/stopwatchapp.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 16,
+    name: "Language Guesser",
+    desc: "A Simple language gusser in express.js/node.js using franc and langs",
+    image: "/images/projects/languageguesser.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 17,
+    name: "Score Keeper",
+    desc: "A Simple JavaScript, Css and HTML website with score keeping and Reset functionality.",
+    image: "/images/projects/scorekeeper.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 18,
+    name: "Lovely Project",
+    desc: "A beautiful project with video background fully responsive with Bootstrap and Media queries.",
+    image: "/images/projects/lovelyproject.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 19,
+    name: "Fruit game",
+    desc: "It is a very low budget rip off of fruit ninja with sounds and score keeping.",
+    image: "/images/projects/fruitninja.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 20,
+    name: "Math game",
+    desc: "It is a basic maths multiplication game using javaScript. It has a timer and scorekeeper",
+    image: "/images/projects/math.png",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+];
